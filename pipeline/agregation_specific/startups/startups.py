a15d6c0785620e6fe64a7f6f1110b35b84b48b3d
AlexandraUrusova <60105930+AlexandraUrusova@users.noreply.github.com>
2021-06-19 17:24:08 +0300

    Update startups.py

--- pipeline/agregation_specific/startups/startups.py
+++ pipeline/agregation_specific/startups/startups.py
@@ -1,36 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-
 import pandas as pd
 
 session = requests.session()
 
-response = session.get("https://www.greenlight.guru/blog/top-100-medical-device-startups")
+url = 'https://www.greenlight.guru/blog/top-100-medical-device-startups'
 
-if response.status_code == 200:
-    print("Success")
-else:
-    print("Bad result")
+def get_startups_2020_rounded_info(url):
+    response = session.get(url)
 
+    if response.status_code == 200:
+        print("Success")
+    else:
+        print("Bad result")
 
-column_names = ['Startup name', 'Year founded', 'Total money raised', 'Amount raised in last round',
-'Announcement date of last round', 'Number of employees', 'Website', 'Description']
+    column_names = ['Startup name', 'Year founded', 'Total money raised', 'Amount raised in last round',
+    'Announcement date of last round', 'Number of employees', 'Description']
 
-df_startups = pd.DataFrame(columns = column_names)
+    df_startups = pd.DataFrame(columns = column_names)
 
+    soup = BeautifulSoup(response.text, 'html.parser')
+    elements = soup.find_all(['h3', 'p'])
 
-soup = BeautifulSoup(response.text, 'html.parser')
-elements = soup.find_all(['h3', 'p'])
+    for i in range(len(elements)):
+        if 'Year founded' in elements[i].text:
+            name = elements[i-1].text
+            description = elements[i+6].text
+            append_string = [name] + [element.split(':')[1] for element in [elements[i].text, elements[i+1].text, elements[i+2].text,
+                              elements[i+3].text, elements[i+4].text]] + [description]
+            a_series = pd.Series(append_string, index = df_startups.columns)
+            df_startups = df_startups.append(a_series, ignore_index=True)
+    return df_startups
+    
 
-for i in range(len(elements)):
-    if 'Year founded' in elements[i].text:
-        #print(elements[i])
-        append_string =  [elements[i-1].text, elements[i].text, elements[i+1].text, elements[i+2].text,
-                          elements[i+3].text, elements[i+4].text, elements[i+5].text, elements[i+6].text]
-        #print(append_string)
-        a_series = pd.Series(append_string, index = df_startups.columns)
-        df_startups = df_startups.append(a_series, ignore_index=True)
-        #print(append_string)
-        
-        
-df_startups
+print(get_startups_2020_rounded_info(url))
